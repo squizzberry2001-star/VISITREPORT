7c85ca6c4739424112225fc5aca4b4989ee659eb
squizzberry2001-star <squizzberry2001@gmail.com>
2026-05-01 13:35:38 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'revamp41-home-floating-linked-device';
+const APP_VERSION = 'revamp42-linked-device-convex-rules';
 const CACHE_NAME = `bestie-visit-${APP_VERSION}`;
 const LOCAL_ASSETS = [
   './src/theme.css',
