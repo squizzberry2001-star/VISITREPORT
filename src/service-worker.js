10f965db70be226c93ebf0714771b589cdcf0c03
squizzberry2001-star <squizzberry2001@gmail.com>
2026-05-05 11:36:21 +0700

    Add files via upload

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'revamp89-email-remove-auto-note';
+const APP_VERSION = 'revamp92-hide-clear-topnav-redownload';
 const CACHE_NAME = `bestie-visit-${APP_VERSION}`;
 const LOCAL_ASSETS = [
   './src/theme.css',
