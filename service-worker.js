01fcdc2ba921030b3188e350e5128f9426209184
squizzberry2001-star <squizzberry2001@gmail.com>
2026-05-07 06:39:39 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'revamp217-email-history-admin-stable';
+const APP_VERSION = 'revamp219-soft-footer-watermark';
 const CACHE_NAME = `bestie-visit-${APP_VERSION}`;
 const LOCAL_ASSETS = [
   './src/theme.css',
