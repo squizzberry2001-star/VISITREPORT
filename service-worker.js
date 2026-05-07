8de6f74cedbf8bdbc91a34c824146ceab3cce2b6
squizzberry2001-star <squizzberry2001@gmail.com>
2026-05-07 21:00:13 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'revamp247-pdf-preview-input-data-sync';
+const APP_VERSION = 'revamp248-preview-input-pinch-desktop-stable';
 const CACHE_NAME = `bestie-visit-${APP_VERSION}`;
 const LOCAL_ASSETS = [
   './src/theme.css',
