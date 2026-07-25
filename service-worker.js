3ecb148733919fc9802487921f700ca982a7f671
AANBAGUS <squizzberry2001@gmail.com>
2026-07-25 16:03:51 +0700

    fix: inject custom tailwind classes to theme.css since compiler is missing

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = 'revamp281-enterprise-features-v45';
-const CACHE_NAME = `bestie-visit-revamp281-enterprise-features-v45`;
+const APP_VERSION = 'revamp281-enterprise-features-v46';
+const CACHE_NAME = `bestie-visit-revamp281-enterprise-features-v46`;
 const LOCAL_ASSETS = [
   './src/theme.css',
   './src/tailwind.generated.css',
