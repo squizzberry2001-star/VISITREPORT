047f51807ea87f240d22404725035f3f205bca81
AANBAGUS <squizzberry2001@gmail.com>
2026-07-26 15:23:16 +0700

    chore(config): apply user Firebase config keys

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
 // Firebase configuration for Bestie Visit
-// Ganti nilai-nilai di bawah ini dengan konfigurasi dari Firebase Console Anda (Project Settings > General > Your apps)
 window.RB_FIREBASE_CONFIG = {
   enabled: true,
   firebaseConfig: {
-    apiKey: "ISI_API_KEY_ANDA_DI_SINI",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID"
+    apiKey: "AIzaSyDJRe7Rfgtev2Wvm25q6ol1I-EC_dCbc4c",
+    authDomain: "visit-bestie.firebaseapp.com",
+    projectId: "visit-bestie",
+    storageBucket: "visit-bestie.firebasestorage.app",
+    messagingSenderId: "406727156945",
+    appId: "1:406727156945:web:20a690f986468fa3dcd1f0",
+    measurementId: "G-W8L7D8TMJJ"
   },
 
   // Collections mapping
