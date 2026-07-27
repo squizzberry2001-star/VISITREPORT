d6273db84ff4ed88878ce4e8509ba0296989fe4d
AANBAGUS <squizzberry2001@gmail.com>
2026-07-27 22:39:49 +0700

    fix: correctly apply brand colors to src/theme.css as the previous attempt incorrectly modified the root theme.css

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = 'revamp327-modular-v66';
-const CACHE_NAME = `bestie-visit-revamp327-modular-v66`;
+const APP_VERSION = 'revamp327-modular-v67';
+const CACHE_NAME = `bestie-visit-revamp327-modular-v67`;
 const LOCAL_ASSETS = [
   './src/theme.css',
   './src/tailwind.generated.css',
