4046705f2be3f5738157898e44111cb627f693f4
AANBAGUS <squizzberry2001@gmail.com>
2026-07-28 19:26:44 +0700

    chore: bump version to v90 to force cache clear and put main on top

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = 'revamp327-modular-v89';
-const CACHE_NAME = `bestie-visit-revamp327-modular-v89`;
+const APP_VERSION = 'revamp327-modular-v90';
+const CACHE_NAME = `bestie-visit-revamp327-modular-v90`;
 const LOCAL_ASSETS = [
   './src/theme.css',
   './src/tailwind.generated.css',
